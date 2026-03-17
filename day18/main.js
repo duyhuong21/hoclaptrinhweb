d465929b09cf5d4769944bdfc32c7a94f4bdff47
huong <tdh21@gmail.com>
2026-03-17 14:31:00 +0700

    day

--- day18/main.js
+++ day18/main.js
@@ -1,21 +1,26 @@
 // console.log("Hello World!");
 // console.log("oke ");
-// // khai bao biem
+
+// // khai bao bien ( declare )
 // let x, y, z
 // x = "oke anh nho"
-// // khoi tao bien
+
+// // khoi tao bien (init)
 // const a = 1
 // let b  = 2
 // var c = 3
-//
+
 // console.log(a, b, c, x, y, z)
 
-let a=10
-console.log(typeof a)
+// let a = 100
+// console.log(a)
+//
+// const isNumber = true
+// console.log(isNumber)
+//
+// const name = "Huong"
+// console.log(name)
 
-const isNumber=true
-console.log(isNumber)
-
-const name="duyhuong"
-console.log(name)
-
+const numbers = [4,3,1,5,1]
+console.log(numbers[0])
+console.log(numbers[4])
